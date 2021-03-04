49b71ca4676e20085ef832c67e5f0725c2b2c5f3
Andrés <andres.gerz@gmail.com>
2021-03-04 16:15:49 -0300

    I added laravel.

---
+++ api/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+ 
+    // User Register
+    public function register(Request $request) {
+        $validator  =   Validator::make($request->all(), [
+            "name"  =>  "required",
+            "email"  =>  "required|email",
+            "phone"  =>  "required",
+            "password"  =>  "required"
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(["status" => "failed", "validation_errors" => $validator->errors()]);
+        }
+
+        $inputs = $request->all();
+        $inputs["password"] = Hash::make($request->password);
+
+        $user   =   User::create($inputs);
+
+        if(!is_null($user)) {
+            return response()->json(["status" => "success", "message" => "Success! registration completed", "data" => $user]);
+        }
+        else {
+            return response()->json(["status" => "failed", "message" => "Registration failed!"]);
+        }       
+    }
+
+    // User login
+    public function login(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            "email" =>  "required|email",
+            "password" =>  "required",
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(["validation_errors" => $validator->errors()]);
+        }
+
+        $user           =       User::where("email", $request->email)->first();
+
+        if(is_null($user)) {
+            return response()->json(["status" => "failed", "message" => "Failed! email not found"]);
+        }
+
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user       =       Auth::user();
+            $token      =       $user->createToken('token')->plainTextToken;
+
+            return response()->json(["status" => "success", "login" => true, "token" => $token, "data" => $user]);
+        }
+        else {
+            return response()->json(["status" => "failed", "success" => false, "message" => "Whoops! invalid password"]);
+        }
+    }
+
+    
+    // User Detail
+    public function user() {
+        $user       =       Auth::user();
+        if(!is_null($user)) { 
+            return response()->json(["status" => "success", "data" => $user]);
+        }
+
+        else {
+            return response()->json(["status" => "failed", "message" => "Whoops! no user found"]);
+        }        
+    }
+}
