095c5040839c6d3472c4e02a5c2774d168b65851
Andrés <andres.gerz@gmail.com>
2021-03-09 10:30:42 -0300

    I consumed my api.

--- src/components/ForecastTable.js
+++ src/components/ForecastTable.js
@@ -3,102 +3,107 @@ import moment from "moment";
 import { useState, useEffect, useContext } from 'react'
 import axios from 'axios'
 import { DataContext } from '../context/Context'
+import { 
+  Cloud, 
+  CloudSun, 
+  BrightnessHigh, 
+  Hurricane, 
+  TropicalStorm,
+  Tornado,
+  Tsunami,
+  CloudLightningRain,
+  CloudLightning,
+  CloudHail,
+  CloudSnow,
+  Wind,
+  CloudSleet
+} from 'react-bootstrap-icons';
 
 import './ForecastTable.module.scss'
 
-import { isArray } from 'util';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import {
-  faSun,
-  faMoon,
-  faCloud,
-  faCloudRain,
-  faCloudSun,
-  faCloudSunRain,
-  faCloudShowersHeavy,
-  faCloudMoon,
-  faCloudMoonRain,
-  faPooStorm,
-  faCloudMeatball
+const BootstrapIcon = ({icon, size, color}) => {
+                                                                                 switch(icon) {
+                                                                               
+    case 'Thunderstorms':
+    case 'Thundershowers': 
+    case 'Storm':
+      return <CloudLightningRain
+      size={size} color={color} />;
+    case 'Lightning':
+      return <CloudLightning
+      size={size} color={color} />;
+    case 'Hail':
+      return <CloudHail
+      size={size} color={color} />;
+    case 'Blowing Snow': 
+    case 'Blizzard': 
+    case 'Snowdrift': 
+    case 'Snowstorm':
+      return <>
+      <Wind
+      size={size} color={color} />
+      <CloudSnow
+      size={size} color={color} />
+      </> ;
+    case 'Snow Showers':
+    case 'Flurries':
+    case 'Snow':
+    case 'Heavy Snow':
+    case 'Snowfall':
+    case 'Light Snow':  
+      return <CloudSnow
+      size={size} color={color} />;
+    case 'Sleet':  
+      return <CloudSleet
+      size={size} color={color} />;
+    case 'Cloudy':
+    case 'Mostly Cloudy': 
+      return <Cloud size={size} color={color} />;
+    case 'Sunny': 
+    case 'Periods':
+    case 'Partly Cloudy': 
+    case 'Partly Bright': 
+    case 'Mild':
+      return <CloudSun size={size} color={color} />;
+    case 'Bright':
+    case 'Sunny':
+    case 'Fair':
+    case 'Fine': 
+    case 'Clear':
+      return <BrightnessHigh size={size} color={color} />;
+    case 'hurricane':
+      return <Hurricane size={size} color={color} />;
+    case 'tropical-storm':
+      return <TropicalStorm size={size} color={color} />;
+    case 'tornado':
+      return <Tornado size={size} color={color} />;
+    case 'tsunami':
+      return <Tsunami size={size} color={color} />;
+    default:
+      return null;
+  }
+}
 
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { type } from "os";
-import { createContext } from "vm";
-
-library.add(
-  fab,
-  faSun,
-  faMoon,
-  faCloud,
-  faCloudRain,
-  faCloudSun,
-  faCloudSunRain,
-  faCloudShowersHeavy,
-  faCloudMoon,
-  faCloudMoonRain,
-  faPooStorm,
-  faCloudMeatball
-)
-
-
-const days = [ 
-  [moment().day(0).locale("en").format("dddd"), 
-  moment().day(0).locale("en").format("DD MMM")],
-  moment().day(1).locale("en").format("dddd"),
-  moment().day(2).locale("en").format("dddd"),
-  moment().day(3).locale("en").format("dddd"),
-  moment().day(4).locale("en").format("dddd")
-]
-
-
-const ForecastTable = () => {
+const ForecastTable = ({}) => {
 
   const { state } = useContext(DataContext);
   const [datas, setDatas] = useState([]);
   const [city, setCity] = useState('');
   
-
-  const convertToIcons = (id) => {
-  
-    const icons = {
-      "clear sky": faSun,
-      "scattered clouds": faSun,
-      "0": faMoon,
-      "overcast clouds": faCloud,
-      "light rain": faCloudRain,
-      "moderate rain": faCloudRain,
-      "broken clouds": faCloudSun,
-      "few clouds": faCloudSun,
-      "5": faCloudSunRain,
-      "7": faCloudShowersHeavy,
-      "8": faCloudMoon,
-      "9": faCloudMoonRain,
-      "10": faPooStorm,
-      "snow": faCloudMeatball,
-      "light snow": faCloudMeatball
-    }
-  
-    return icons[id];
+  const convertDate = (date) => {
+    return moment.parseZone(date).format("dddd");
   }
-  
 
   useEffect( () => {
     
-    const URL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
-    const KEY = '&units=metric&appid=6200f7fd2611fa3c695ade64a041d5f7';
-    
-    axios.get(URL + state.city + KEY)
+    axios.get('http://weather-api.lndo.site/api/placeholder')
       .then(result => {
+        console.log(result);
+        let dataCity = result.data.city; 
 
-        setCity(result.data.city.name + ", " + result.data.city.country)
-        setDatas(result.data.list.filter(objectDay => {
-          return String(objectDay.dt_txt).split(" ")[1] === "12:00:00";
-          }))
-
-        }
-        )
+        setCity(`${dataCity.cityName}, ${dataCity.member.memName}`);
+        setDatas(dataCity.forecast.forecastDay);
+        })
         .catch(err => console.log(err));
         
       }, [state.city])
@@ -107,30 +112,29 @@ const ForecastTable = () => {
       return(
         <ul className="table-container">
         {
-        
-        datas.map((day,index) => {
+          datas.map((day,index) => {
             
           if (index === 0) {
             return(
               <li className="days" key={String(index)}>
                 <div className="top">
-                <div className="today-date">{days[index][0]}</div>
-                  <div className="today-day">{days[index][1]}</div>
+                <div className="today-date">{convertDate(day.forecastDate)}</div>
+                  <div className="today-day">{moment.parseZone(day.forecastDate).format("D MMM")}</div>
                 </div>
                 
                 <div className="bottom">
                   <div className="today-data">
                     <p className="city">{city}</p>
-                    <h1>{day.main.temp.toFixed(1) + " °C"}</h1>
-                    <p>{day.main.pressure + " hPa"}</p>
-                    <p>{day.main.humidity + " %"}</p>
-                    <p>{day.wind.speed.toFixed(1) + " km/h"}</p>  
+                    <h2>{`°C`}</h2>
+                    <p>{" hPa"}</p>
+                    <p>{" %"}</p>
+                    <p>{" km/h"}</p>  
                   </div>
                   <div className="icon-today-wrapper">
-                    <FontAwesomeIcon
-                      icon={convertToIcons(day.weather[0].description)}
-                      size="1x"
-                      className="icon-today"
+                    <BootstrapIcon
+                      icon={day.weather}
+                      size={84}
+                      color={'#00a6d4'}
                     />
                   </div>
                 </div>
@@ -138,32 +142,28 @@ const ForecastTable = () => {
               </li>)
             
           } else {  
-             return (
-               <li className="days" key={String(index)}>
-                <div className="top"><p>{days[index]}</p></div>
-                <div className="bottom">
-                  <div className="icons-week-wrapper">
-                    <FontAwesomeIcon
-                      icon={convertToIcons(day.weather[0].description)}
-                      size="1x"
-                      className="icons-week"
-                    />
+              return (
+                <li className="days" key={String(index)}>
+                  <div className="top"><p>{convertDate(day.forecastDate)}</p></div>
+                  <div className="bottom">
+                    <div className="icons-week-wrapper">
+                      <BootstrapIcon
+                        icon={day.weather}
+                        size={42}
+                        color={'#00a6d4'}
+                      />
+                    </div>
+                    <div className="temperatures-week">
+                      <p className="temperatures-max">{day.maxTemp + " °C"}</p>
+                      <p className="temperatures-min">{day.minTemp + " °C"}</p>
+                    </div>
                   </div>
-                  <div className="temperatures-week">
-                    <p className="temperatures-max">{day.main.temp_max.toFixed(1) + " °C"}</p>
-                    <p className="temperatures-min">{day.main.temp_min.toFixed(1) + " °C"}</p>
-                  </div>
-                </div>
-              </li>)
+                </li>)
         }
-      }
-      )
+      })
       }
     </ul>
   )
-
-  
 }
-
 
 export default ForecastTable;
