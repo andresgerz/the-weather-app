a088960b71025ff7a6a888bfdd83060878375150
Andrés <andres.gerz@gmail.com>
2022-11-20 20:07:57 -0300

    feat: WIP.

---
+++ api-drupal/sites/default/files/php/twig/62a8d754b0b3f_image-widget.html.twig_sljKJyEdoW1ASbSh5SZUCURPe/ROo7SZD1Z2IwPKHXiB_EDeVm0PklEeH0BKPjcJ6KDaU.php
@@ -0,0 +1,113 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/bartik/templates/classy/content-edit/image-widget.html.twig */
+class __TwigTemplate_31f49d7433ee6d93502ca108fa9478a24548b5ecc8fa5f29bed8fafbac865947 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 13
+        echo "<div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 13, $this->source), "html", null, true);
+        echo ">
+  ";
+        // line 14
+        if (twig_get_attribute($this->env, $this->source, ($context["data"] ?? null), "preview", [], "any", false, false, true, 14)) {
+            // line 15
+            echo "    <div class=\"image-preview\">
+      ";
+            // line 16
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["data"] ?? null), "preview", [], "any", false, false, true, 16), 16, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 19
+        echo "  <div class=\"image-widget-data\">
+    ";
+        // line 21
+        echo "    ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter($this->sandbox->ensureToStringAllowed(($context["data"] ?? null), 21, $this->source), "preview"), "html", null, true);
+        echo "
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bartik/templates/classy/content-edit/image-widget.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  58 => 21,  55 => 19,  49 => 16,  46 => 15,  44 => 14,  39 => 13,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/bartik/templates/classy/content-edit/image-widget.html.twig", "/app/core/themes/bartik/templates/classy/content-edit/image-widget.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 14);
+        static $filters = array("escape" => 13, "without" => 21);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape', 'without'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
